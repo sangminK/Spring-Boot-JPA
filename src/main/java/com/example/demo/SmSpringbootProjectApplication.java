cfdc3428a59824a79cc986c81f2e606e93d5ca7b
김상민 <smkim@gaeasoft.co.kr>
2019-03-28 15:47:58 +0900

    FIRST

---
+++ src/main/java/com/example/demo/SmSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmSpringbootProjectApplication.class, args);
+	}
+
+}
